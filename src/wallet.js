c0a8dca5723df3cb167ff8f454f8b0f790c0167d
TheWildHustle <145122833+TheWildHustle@users.noreply.github.com>
2024-09-06 12:11:24 -0400

    Create file src/wallet.js

---
+++ src/wallet.js
@@ -0,0 +1,31 @@
+import { NostrWalletConnect } from './nip47';
+
+let walletConnect = null;
+
+export async function connectWallet(connectionUri) {
+  walletConnect = new NostrWalletConnect(connectionUri);
+  await walletConnect.connect();
+}
+
+export async function disconnectWallet() {
+  if (walletConnect) {
+    await walletConnect.disconnect();
+    walletConnect = null;
+  }
+}
+
+export async function payInvoice(invoice, amount) {
+  if (!walletConnect) {
+    throw new Error('Wallet not connected');
+  }
+  return walletConnect.payInvoice(invoice, amount);
+}
+
+export async function getBalance() {
+  if (!walletConnect) {
+    throw new Error('Wallet not connected');
+  }
+  return walletConnect.getBalance();
+}
+
+// Add more wallet-related functions as needed
